86bcd94ed72db66ea49dc650e5d8b5805001ee6c
Ton Machielsen <ton.machielsen@lacework.net>
2021-10-30 17:01:49 +0200

    Initial commit

--- hello-instance/main.tf
+++ hello-instance/main.tf
@@ -5,7 +5,23 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-091f21ecba031b39a"
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.ssh.key_name
   tags = {
     Name = "HelloInstance"
   }
+}
+
+resource "random_id" "id" {
+  byte_length = 4
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "aws_key_pair" "ssh" {
+  key_name   = "${random_id.id.hex}-ssh"
+  public_key = tls_private_key.ssh.public_key_openssh
+  tags       = {}
 }
